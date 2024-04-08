6de5ea69442f1096158a99aecb72d43be49786d0
harryypham <phamharrytdn@gmail.com>
2024-04-08 17:08:43 +1000

    cover image

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -29,7 +29,10 @@ export const CoverImageModal = () => {
             setFile(file)
 
             const res = await edgestore.publicFiles.upload({
-                file
+                file,
+                options: {
+                    replaceTargetUrl: coverImage.url
+                }
             })
 
             await update({
--- hooks/use-cover-image.tsx
+++ hooks/use-cover-image.tsx
@@ -2,13 +2,17 @@ import { SettingsIcon } from "lucide-react"
 import {create } from "zustand"
 
 type CoverImageStore = {
-    isOpen: boolean,
-    onOpen: () => void,
-    onClose: () => void,
+    url?: string;
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+    onReplace: (url : string) => void;
 }
 
 export const useCoverImage = create<CoverImageStore>((set) => ({
+    url: undefined,
     isOpen: false,
     onOpen: () => set({isOpen: true}),
-    onClose: () => set({ isOpen: false})
+    onClose: () => set({ isOpen: false, url: undefined}),
+    onReplace: (url : string) => set({ isOpen: true, url})
 }))
